bdeb1794598fb0f70e68d665d1802bc92245b2e0
Parikshith Sivakumar <115164293+parikshithsivakumar@users.noreply.github.com>
2025-05-11 20:04:36 +0530

    Update Support.js

--- client/src/components/Pages/Support.js
+++ client/src/components/Pages/Support.js
@@ -25,7 +25,7 @@ const Support = () => {
     };
 
     // Send feedback data to the backend API
-    fetch('http://localhost:5000/feedback/submit-feedback', {
+    fetch('${process.env.REACT_APP_API_URL}/feedback/submit-feedback', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'

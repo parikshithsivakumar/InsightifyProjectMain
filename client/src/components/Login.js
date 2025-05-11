62d25c41537b551593e88f231d043d255457897a
Parikshith Sivakumar <115164293+parikshithsivakumar@users.noreply.github.com>
2025-05-11 19:32:24 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,7 +25,7 @@ export default function Login() {
     setSuccess('');
 
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+     const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
